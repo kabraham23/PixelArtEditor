f68a1055d4ef33611864e3a1a0d65bf5c7319d5f
Katie A <katie.t.abraham@gmail.com>
2021-08-26 14:19:35 -0400

    adding dom

--- main.js
+++ main.js
@@ -23,3 +23,13 @@ class Picture {
 function updateState(state, action) {
     return Object.assign({}, state, action);
 }
+
+function elt(type, props, ...children) {
+    let dom = document.createElement(type);
+    if (props) Object.assign(dom, props);
+    for (let child of children) {
+        if (typeof child != "string") dom.appendChild(child);
+        else dom.appendChild(document.createTextNode(child));
+    }
+    return dom;
+}
